41cc5f1a01c2dfda92cae046868226c321095868
Marek Suchánek <marek.suchanek@protonmail.com>
2019-06-28 23:17:15 +0000

    Fix the bash prompt not to break line editing

--- dot/bash_snippet.sh
+++ dot/bash_snippet.sh
@@ -1,18 +1,9 @@
 # A simple, colorized prompt: 
 #   <user name> <working dir base name> $
 
-# Save color codes into sane variables
-blue="\e[1;34m"
-red="\e[1;31m"
-cyan="\e[1;36m"
-
 # Use red for root, blue for user
 if [[ $UID -eq 0 ]]; then
-    fgcolor=$red
+    export PS1="\[\e[1;31m\]\u\[\e[m\] \[\e[1;36m\]\W\[\e[m\] \[\e[1;31m\]\\$\[\e[m\] "
 else
-    fgcolor=$blue
-fi  
-
-# Complete prompt
-export PS1="${fgcolor}\u ${cyan}\W ${fgcolor}\$ \e[m"
-
+    export PS1="\[\e[1;34m\]\u\[\e[m\] \[\e[1;36m\]\W\[\e[m\] \[\e[1;34m\]\\$\[\e[m\] "
+fi
